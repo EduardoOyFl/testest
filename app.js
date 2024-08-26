edff3bec837fa845b9f28dd68c72ce61c5ced829
Eduardo Oyarzo <Eduardo.oyfl@gmail.com>
2024-08-26 19:24:50 -0400

    modificacion

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-let X = document.querySelector(".ggg");
+/*let X = document.querySelector(".ggg");
 
 X.onClick = alert("Como estas");
 function saluda(){
     alert("Como estas hoy");
+}
+*/
+
+function saluda(){
+    alert("Hola te saludo");
 }
